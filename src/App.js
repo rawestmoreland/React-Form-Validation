dc47250155c3a2c07ee9c2527637fde7b7de1b81
Richard Westmoreland <kegtrack@gmail.com>
2019-09-16 09:42:48 -0500

    validation finished

--- src/App.js
+++ src/App.js
@@ -1,36 +1,83 @@
-import React, { Component } from 'react';
-import './App.css';
-import LoginForm from './components/LoginForm/LoginForm'
+import React, { Component } from "react";
+import "./App.css";
+import LoginForm from "./components/LoginForm/LoginForm";
+
+// TODO: signup form state to the signup form
 
 class App extends Component {
-
   state = {
     email: "",
     password: "",
     repeatPass: "",
-    validated: false,
-    empty: false,
-    validEmail: true,
-    passesMatch: true
-  }
+    valid: false,
+    errors: {
+      emailError: "",
+      passwordError: "",
+      repeatPassError: "",
+      passMatchError: ""
+    }
+  };
 
-  handleSubmit = (e) => {
+  handleSubmit = e => {};
+
+  handleChange = e => {
+    e.preventDefault();
+
+    // Regex for a valid email address
+    const validEmailRegex = RegExp(
+      /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+    );
+
+    // Destructure the event
+    const { name, value } = e.target;
+
+    // Copy of the errors state to change within the switch statement
+    let errors = this.state.errors;
+
+    // Check each field for errors
+    switch (name) {
+      case "email":
+        errors.emailError =
+          validEmailRegex.test(value) || !value.length > 0
+            ? ""
+            : "email is not valid";
+        break;
+      case "password":
+        errors.passwordError =
+          !value.length > 0 || value.length > 6
+            ? ""
+            : "password must be at least 6 characters";
+        break;
+      case "repeatPass":
+        errors.passMatchError = (!value.length > 0 || value === this.state.password) ? "" : "passwords do not match"
+        break;
+      default:
+        break;
+    }
+
+    // If there are no errors, set the valid state to true to enable the submit button
+    if (errors.emailError === "" && errors.passMatchError === "" && errors.passwordError === "") {
+      this.setState({
+        valid: true
+      })
+    }
     
-  }
-
-  handleChange = (e) => {
-    const {name, value} = e.target
+    // Set the state with each keystroke
     this.setState({
       [name]: value
-    })
-  }
+    });
+  };
 
   render() {
     return (
       <div className="App">
-        <LoginForm handleSubmit={this.handleSubmit} handleChange={this.handleChange} data={this.state} />
+        <LoginForm
+          handleSubmit={this.handleSubmit}
+          handleChange={this.handleChange}
+          data={this.state}
+        />
       </div>
-    )
+    );
   }
 }
 
--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -3,19 +3,30 @@ import './LoginForm.css'
 import LoginFormErrors from './LoginFormErrors'
 
 const LoginForm = props => {
+
     return (
         <div className="signup-form-wrapper">
-            <div>
-
+            <div className="signup-title">
+                Create Account
             </div>
-            <form onSubmit={props.handleSubmit} className="signup-form">
-                <label>email</label>
-                <input onChange={props.handleChange} type="email" value={props.data.email} name="email" />
-                <label>password</label>
-                <input onChange={props.handleChange} type="password" value={props.data.password} name="password" />
-                <label>re-enter password</label>
-                <input onChange={props.handleChange} type="password" value={props.data.repeatPass} name="repeatPass" />
-                <button type="submit">submit</button>
+            <form onSubmit={props.handleSubmit} className="signup-form" noValidate>
+                <div className="row">
+                    <label>email</label>
+                    <input onChange={props.handleChange} type="email" value={props.data.email} name="email" noValidate />
+                    <label className="signup-form-error">{props.data.errors.emailError}</label>
+                </div>
+                <div className="row">
+                    <label>password</label>
+                    <input onChange={props.handleChange} type="password" value={props.data.password} name="password" noValidate />
+                    <label className="signup-form-error">{props.data.errors.passwordError}</label>
+                </div>
+                <div className="row">
+                    <label>re-enter password</label>
+                    <input onChange={props.handleChange} type="password" value={props.data.repeatPass} name="repeatPass" noValidate />
+                    <label className="signup-form-error">{props.data.errors.passMatchError}</label>
+                </div>
+                <small>Already have an account?</small>
+                <button disabled={!props.data.valid} type="submit">Create</button>
             </form>
         </div>
     )
